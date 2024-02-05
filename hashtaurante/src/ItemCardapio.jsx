929afb1c96461dcbdf6f40ffafe33a5e16312527
Fabio Costa <fabio.fc@hotmail.com>
2024-02-05 14:48:29 -0300

    Projeto Cardapio

---
+++ hashtaurante/src/ItemCardapio.jsx
@@ -0,0 +1,10 @@
+export function Itemcardapio(props) {
+    return <div className="container-item-cardapio">
+        <div>
+            <h2>{props.nome}</h2>
+            <p>{props.preco}</p>
+            <p>{props.descricao}</p>
+        </div>
+        <img src={props.imagem} alt="" />
+    </div>
+}
